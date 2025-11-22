70f3ead8b6fd50305251774a2d61470429b8c7a4
wall4cee <pro.reset222@passfwd.com>
2025-11-22 00:32:42 +0000

    Second commit - Project OK

---
+++ src/content/projects.js
@@ -0,0 +1,12 @@
+export const projects = [
+  {
+    title: "[Dev-Web] Solena Hypnose",
+    desc: "Showcase website for a hypnotherapy practice (Work in progress)",
+    link: "#"
+  },
+  {
+    title: "[Cyber] Scripts de Cryptography",
+    desc: "Small tool to decode XOR encryption",
+    link: "#"
+  }
+];
---
+++ src/content/writeups.js
@@ -0,0 +1,12 @@
+export const writeups = [
+  {
+    title: "Room HackTheBox - Web exploitation (Plugin Wordpress, Nmap, Metasploit)",
+    platform: "HackTheBox",
+    link: "#"
+  },
+  {
+    title: "CTF StarHack 2025",
+    platform: "CTF On Site",
+    link: "#"
+  }
+];
