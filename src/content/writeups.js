83af6443b219b06dba704e2aba3d2f4151e01622
wall4cee <pro.reset222@passfwd.com>
2026-02-07 15:28:19 +0100

    add new writeup

--- src/content/writeups.js
+++ src/content/writeups.js
@@ -1,6 +1,6 @@
 export const writeups = [
   {
-    title: "Room HackTheBox - Web exploitation (Plugin Wordpress, Nmap, Metasploit)",
+    title: "HTB Academy - Web exploitation (Plugin Wordpress, Nmap, Metasploit)",
     platform: "HackTheBox",
     link: "https://github.com/wall4cee/writeups/tree/main/htb/web-exploit-basics/htb_writeup_exploitation.md"
   },
@@ -8,5 +8,10 @@ export const writeups = [
     title: "CTF StarHack 2025",
     platform: "CTF On Site",
     link: "https://github.com/wall4cee/writeups/tree/main/ctf/starhack2k25"
+  },
+  {
+    title: "HTB Labs (Medium- Windows)- Overwatch (Reverse-engineering, Active-Directory, MSSQL)",
+    platform: "HackTheBox",
+    link: "https://github.com/wall4cee/writeups/tree/main/htb/overwatch/(MEDIUM-HTB)Overwatch.md"
   }
 ];
